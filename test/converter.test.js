9ad14c6cd60aecbcb1322d9a312bbddc3b4f7b41
Steven T Ancheta <steventancheta@gmail.com>
2016-07-21 21:56:14 -0700

    Finished Tests for Converter.js and replaced " with '
    -There are much fewer test cases now because these functions do not have the
    responsibility for validation.

--- test/converter.test.js
+++ test/converter.test.js
@@ -1,12 +1,40 @@
-var chai = require("chai");
-var converter = require("../lib/converter");
+var chai = require('chai');
+var converter = require('../lib/converter');
 var assert = chai.assert;
 
 describe('Time Converter', function() {
   describe('Converting Unix Time to Natural Language Time', function() {
-
+    it('should return January 19, 2038 when given 2147483647', function() {
+      assert.equal('January 19, 2038', convertUtoN('2147483647'));
+    });
+    it('should return December 13, 1901 when given -2147483648', function() {
+      assert.equal('December 13, 1901', convertUtoN('-2147483648'));
+    });
+    it('should return January 1, 1970 when given 1', function() {
+      assert.equal('January 1, 1970', convertUtoN('1'));
+    });
+    it('should return January 1, 1970 when given 0', function() {
+      assert.equal('January 1, 1970', convertUtoN('0'));
+    });
+    it('should return December 31, 1969 when given -1', function() {
+      assert.equal('December 31, 1969', convertUtoN('-1'));
+    });
   });
   describe('Converting Natural Language Time to Unix Time', function() {
-
+    it('should return 2147472000 when given January 19, 2038', function() {
+      assert.equal('2147472000', convertNtoU('January 19, 2038'));
+    });
+    it('should return -2146003200 when given December 13, 1901', function() {
+      assert.equal('-2146003200', convertNtoU('December 13, 1901'));
+    });
+    it('should return 0 when given January 1, 1970', function() {
+      assert.equal('1', convertNtoU('January 1, 1970'));
+    });
+    it('should return 0 when given January 1, 1970', function() {
+      assert.equal('0', convertNtoU('January 1, 1970'));
+    });
+    it('should return -1 when given December 31, 1969', function() {
+      assert.equal('-1', convertNtoU('December 31, 1969'));
+    });
   });
 });
--- test/validator.test.js
+++ test/validator.test.js
@@ -1,5 +1,5 @@
-var chai = require("chai");
-var validator = require("../lib/validator");
+var chai = require('chai');
+var validator = require('../lib/validator');
 var assert = chai.assert;
 var validateUNIX = validator.validateUNIX;
 var validateNatural = validator.validateNatural;
@@ -10,24 +10,24 @@ describe('Time Validator', function() {
       assert.equal('boolean', typeof(validateUNIX('0')), 'Did not return a boolean');
     });
     it('should return false when date is above max unix time', function() {
-      assert.equal(false, validateUNIX("2147483648"));
+      assert.equal(false, validateUNIX('2147483648'));
     });
     it('should return false when date is below min unix time', function() {
-      assert.equal(false, validateUNIX("-2147483649"));
+      assert.equal(false, validateUNIX('-2147483649'));
     });
     it('should return false when invalid input is entered', function() {
-      assert.equal(false, validateUNIX("1234f"), '1234f has returned true');
-      assert.equal(false, validateUNIX("11111111111"), 'has accepted a number with more than 10 digits');
-      assert.equal(false, validateUNIX("-11111111111"), 'has accepted a negative number with more than 10 digits');
+      assert.equal(false, validateUNIX('1234f'), '1234f has returned true');
+      assert.equal(false, validateUNIX('11111111111'), 'has accepted a number with more than 10 digits');
+      assert.equal(false, validateUNIX('-11111111111'), 'has accepted a negative number with more than 10 digits');
     });
     it('should return true for max unix time', function() {
-      assert.equal(true, validateUNIX("2147483647"));
+      assert.equal(true, validateUNIX('2147483647'));
     });
     it('should return true for min unix time', function() {
-      assert.equal(true, validateUNIX("-2147483648"));
+      assert.equal(true, validateUNIX('-2147483648'));
     });
     it('should return true for int with 1 to 10 digits', function() {
-      var testString = "";
+      var testString = '';
       for(var i = 0; i < 10; i++) {
         testString += '1';
         assert.equal(true, validateUNIX(testString));
@@ -35,7 +35,7 @@ describe('Time Validator', function() {
       }
     });
     it('should return true for negative int with 1 to 10 digits', function() {
-      var testString = "-";
+      var testString = '-';
       for(var i = 0; i < 10; i++) {
         testString += '1';
         assert.equal(true, validateUNIX(testString));
@@ -43,10 +43,10 @@ describe('Time Validator', function() {
       }
     });
     it('should return true for 1469130808', function() {
-      assert.equal(true, validateUNIX("1469130808"));
+      assert.equal(true, validateUNIX('1469130808'));
     });
     it('should return true for -1469130808', function() {
-      assert.equal(true, validateUNIX("-1469130808"));
+      assert.equal(true, validateUNIX('-1469130808'));
     });
   });
   describe('Validating Natural Time', function() {
@@ -54,29 +54,29 @@ describe('Time Validator', function() {
       assert.equal('boolean', typeof(validateNatural('0')), 'Did not return a boolean');
     });
     it('should return false when date is above max unix time', function() {
-      assert.equal(false, validateNatural("January 20, 2038"), "Accepts Date above max date");
+      assert.equal(false, validateNatural('January 20, 2038'), 'Accepts Date above max date');
     });
     it('should return false when date is below min unix time', function() {
-      assert.equal(false, validateNatural("December 12, 1901"), "Accepts Date below min date");
+      assert.equal(false, validateNatural('December 12, 1901'), 'Accepts Date below min date');
     });
     it('should return false when invalid input is entered', function() {
-      assert.equal(false, validateNatural("asdfhg"), "accepted Mary 13, 2013");
-      assert.equal(false, validateNatural("Mary 13, 2013"), "accepted Mary 13, 2013");
+      assert.equal(false, validateNatural('asdfhg'), 'accepted Mary 13, 2013');
+      assert.equal(false, validateNatural('Mary 13, 2013'), 'accepted Mary 13, 2013');
     });
     it('should return true for max unix time', function() {
-      assert.equal(true, validateNatural("January 19, 2038"));
+      assert.equal(true, validateNatural('January 19, 2038'));
     });
     it('should return true for min unix time', function() {
-      assert.equal(true, validateNatural("December 13, 1901"));
+      assert.equal(true, validateNatural('December 13, 1901'));
     });
     it('should return true for july 21, 2016', function() {
-      assert.equal(true, validateNatural("july 21, 2016"), "does not work for all lowercase month");
+      assert.equal(true, validateNatural('july 21, 2016'), 'does not work for all lowercase month');
     });
     it('should return true for JULY 21, 2016', function() {
-      assert.equal(true, validateNatural("JULY 21, 2016"), "does not work for all uppercase month");
+      assert.equal(true, validateNatural('JULY 21, 2016'), 'does not work for all uppercase month');
     });
     it('should return true for June 13, 1923', function() {
-      assert.equal(true, validateNatural("June 13, 1923"));
+      assert.equal(true, validateNatural('June 13, 1923'));
     });
   });
 });
